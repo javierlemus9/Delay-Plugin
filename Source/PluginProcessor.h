b80fa2d83b76f149d5c018b95f86d50a782f5f29
Javier Lemus <javierlemus9@gmail.com>
2025-08-02 16:35:02 -0700

    Finished Chapter 12 code, implemeted feedback feature

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -62,6 +62,9 @@ private:
 
     Parameters params;
     juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> delayLine;
+    
+    float feedbackL = 0.0f;
+    float feedbackR = 0.0f;
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayAudioProcessor)
